a4011cd89ca45974e8b2d45f899aa5447b34a9d4
Basuraj <basurajp135@gmail.com>
2023-11-11 17:44:27 +0530

    variety  added

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 const varietySamplingRate = {
-    'CO-86032': 2.14,
-    'COM-0265': 2.28,
-    'SNK-1374': 3.0,
-    'SNK-13436': 3.50
+    "Co-86032(Nira)": 2.14,
+    "Co-86032(Foundation)": 2.5,
+    "CoM-0265": 2.25,
+    "SNK-13374": 3.0,
+    "SNK-13436": 2.5,
+    "MS-10001": 2.25,
+    "VSI-8005": 2.25,
+    "CoVC-18061": 2.5,
+    "CoVSI-18121": 2.5,
+    "CoC-671": 2.5,
+    "SNK-049": 2.5,
+    "SNK-9293": 2.3,
+    "CoVSI-3102": 2.5,
+    "PDN-15012": 2.6,
+    "Co-92005": 2.5,
+    "VCF-0517": 2.5,
+    "Co-11015": 2.4,
 };
 
-function compute() {
-    let rowSpacing = document.getElementById('rowSpacing').value;
-    let plantSpacing = document.getElementById('plantSpacing').value;
-    let selectedVariety = document.getElementById('sugarcaneType').value;
+function calculateCost() {
+    const rowSpacing = document.getElementById("rowSpacing").value;
+    const plantSpacing = document.getElementById("plantSpacing").value;
+    const selectedVariety = document.getElementById("sugarcaneType").value;
 
-    let rowsPerAcre = 33/rowSpacing;
-    let plantsPerRow = 33/plantSpacing;
+    const rowsPerAcre = 33 / rowSpacing;
+    const plantsPerRow = 33 / plantSpacing;
+    const plantsPerAcre = rowsPerAcre * plantsPerRow * 40;
 
-    let plantsPerAcre = (rowsPerAcre * plantsPerRow) * 40;
-
-    if(selectedVariety in varietySamplingRate) {
-        let totalCost = Math.round(plantsPerAcre * varietySamplingRate[selectedVariety]);
-        document.getElementById("output").innerHTML = `Total Cost = ₹${totalCost}<br>Plants for 1 Acre: ${Math.round(plantsPerAcre)}<br>Price per Plant for '${selectedVariety}' variety: ₹${varietySamplingRate[selectedVariety]}`;
+    if (varietySamplingRate[selectedVariety]) {
+        const totalCost = Math.round(plantsPerAcre * varietySamplingRate[selectedVariety]);
+        document.getElementById("result").innerHTML = `Total Cost = ₹${totalCost}<br/>
+      Plants for 1 Acre: ${Math.round(plantsPerAcre)}<br/>
+      Price per Plant for '${selectedVariety}' variety: ₹${varietySamplingRate[selectedVariety]}`;
     } else {
-        document.getElementById("output").innerHTML = "Please select a valid variety.";
+        document.getElementById("result").innerHTML = "Please select a valid variety.";
     }
 }
 
-function resetFields() {
-    document.getElementById('rowSpacing').value = "";
-    document.getElementById('plantSpacing').value = "";
-    document.getElementById('sugarcaneType').value = "";
-    document.getElementById("output").innerHTML = "";
+function clearInputs() {
+    document.getElementById("rowSpacing").value = "";
+    document.getElementById("plantSpacing").value = "";
+    document.getElementById("sugarcaneType").selectedIndex = 0;
+    document.getElementById("result").innerHTML = "";
 }
+
+document.querySelector('.calculate').addEventListener('click', calculateCost);
+document.querySelector('.reset').addEventListener('click', clearInputs);
